4ac1612782232989c2ab80f9a74e30a2d2e18555
Simone Odegard <Simone.Odegard@gmail.com>
2021-04-25 21:20:28 -0700

    added tests to server.test.js. tests are not working

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -22,4 +22,73 @@ describe('API SERVER:', () => {
     });
   });
 
+  /* ========== create a new item ========== */
+
+  it('should create a new item in the db', async () => {
+    const response = await mockRequest.post('/food').send({ name: 'sushi', calories: 999, type: 'MEAT'})
+    expect(response.status).toBe(201);
+    expect(response.body.calories).toEqual(999);
+  });
+
+  it('should create a new item in the db', async () => {
+    const response = await mockRequest.post('/clothes').send({ name: 'shirt', quantity: 9, size: 'M'})
+    expect(response.status).toBe(201);
+    expect(response.body.name).toEqual('shirt');
+  });
+
+  /* ========== retrieve an item ========== */
+  
+  it('should retrieve an item from the db', async () => {
+    const response = await mockRequest.get('/food/6082076e8f67be8d4e49eff7');
+    expect(response.status).toBe(200);
+    // expect(response.body).toBe(true);
+    console.log(response.body);
+  });
+
+  it('should retrieve an item from the db', async () => {
+    const response = await mockRequest.get('/clothes/1');
+    expect(response.status).toBe(200);
+    // expect(response.body).toBe(true);
+    console.log(response.body);
+  });
+
+  /* ========== retrieve all items ========== */
+
+  it('should retrieve all items from the db', async () => {
+    const response = await mockRequest.get('/food');
+    expect(response.status).toBe(200);
+  });
+
+  it('should retrieve all items from the db', async () => {
+    const response = await mockRequest.get('/clothes');
+    expect(response.status).toBe(200);
+  });
+
+  /* ========== update an item ========== */
+
+  it('should update an item in the db', async () => {
+    const response = await mockRequest.put('/food/1').send({ name: 'sushi', calories: 999, type: 'MEAT'})
+    expect(response.status).toBe(200);
+    expect(response.body.name).toEqual('sushi');
+  });
+
+  it('should update an item', async () => {
+    const response = await mockRequest.put('/clothes/1').send({ name: 'shirt', quantity: 9, size: 'M'})
+    expect(response.status).toBe(200);
+    expect(response.body.name).toEqual('shirt');
+  });
+
+  /* ========== delete an item ========== */
+
+  it('should delete an item in the db', async () => {
+    const response = await mockRequest.delete('/food/1')
+    expect(response.status).toBe(200);
+  });
+
+  it('should delete an item in the db', async () => {
+    // const id = // needs to grab body._id
+    const response = await mockRequest.delete('/clothes/1')
+    expect(response.status).toBe(200);
+  });
+
 })
